9f729ed4c5432fb5849cbd58b7510a5d624228b6
captwase <wasekhan558@gmail.com>
2022-09-10 20:36:56 +0530

    initial commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,12 +11,85 @@ contract Token {
 
 	// Track Balances
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
+	event Transfer(
+		address indexed from,
+		address indexed to,
+		uint256 value
+	);
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+	event Approval(
+		address indexed owner,
+		address indexed spender,
+		uint256 value
+	);
+
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply
+		) 
+	{
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
+
+	function transfer(address _to, uint256 _value)
+		public 
+		returns (bool success)
+	{
+		// Require that sender has enough tokens to spend
+		require(balanceOf[msg.sender] >= _value);
+		
+		_transfer(msg.sender, _to, _value);
+
+		return true;
+	}
+
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value
+	) internal {
+		require(_to != address(0));
+
+		// Deduct tokens from spender
+		balanceOf[_from] = balanceOf[_from] - _value;
+		// Credit tokens to reciever
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		// Emit Event 
+		emit Transfer(msg.sender, _to, _value);
+	}
+
+
+
+	function approve(address _spender, uint256 _value)
+		public
+		returns(bool success)
+	{
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+		function transferFrom(
+			address _from,
+			address _to,
+			uint256 _value
+		)
+			public
+			returns (bool success)
+		{
+
+			
+		}
+
+
 }
