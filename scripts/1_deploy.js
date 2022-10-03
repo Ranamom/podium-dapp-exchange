1721956f5b27dedb76c13f798aef50536534da66
captwase <wasekhan558@gmail.com>
2022-10-03 21:02:57 +0530

    Seed exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,33 @@
 async function main() {
- // Fetch contract to deploy
- const Token = await ethers.getContractFactory("Token")
 
- // Deploy contract
- const token = await Token.deploy()
- await token.deployed()
- console.log('Token deployed to: ${token.address}')
+  console.log(`Preparing for deployment... \n`)
+
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+  const Exchange = await ethers.getContractFactory("Exchange")
+
+  // Fetch accounts
+  const accounts = await ethers.getSigners()
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+  // Deploy contract
+  const dapp = await Token.deploy('Podium Dapp', 'DAPP', '1000000')
+  await dapp.deployed()
+  console.log(`DAPP deployed to: ${dapp.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deployed to: ${exchange.address}`)
+
 }
 
 main()
